0d75875b41aecb07df4ba9bf7b8f4bbd59e71af9
JUAN19742 <jsrueda@puce.edu.ec>
2026-07-13 01:39:49 -0500

    feat(HU-09): agrega funcion obtenerHistorial con filtros por producto y fecha

--- backend/src/routes/movimiento.routes.js
+++ backend/src/routes/movimiento.routes.js
@@ -8,6 +8,5 @@ const {
 
 router.post('/entrada', verificarToken, registrarEntrada);
 router.post('/salida', verificarToken, registrarSalida);
-router.get('/', verificarToken, obtenerHistorial);
 
 module.exports = router;
