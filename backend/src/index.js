57a156480adfc23b95dfbde97835cfcac9fa4423
JUAN19742 <jsrueda@puce.edu.ec>
2026-08-27 00:42:52 -0500

    fix: aumenta el límite de tamaño del body

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const app = express();
 conectarDB();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 
 app.use('/api/auth',     require('./routes/auth.routes'));
 app.use('/api/productos', require('./routes/producto.routes'));
